bb2443d43451290cd9491cac47342605816bb56a
Rusiru Salwathura <rusirusalwathura@icloud.com>
2025-01-18 09:14:41 +0530

    made some changes

---
+++ LMS-cs/LMS-cs/LoginForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace LMS_cs
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(142, 95);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(142, 178);
+            label2.Name = "label2";
+            label2.Size = new Size(111, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(308, 95);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(277, 39);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(308, 178);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(277, 39);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(311, 270);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(285, 62);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
---
+++ LMS-cs/LMS-cs/LoginForm.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace LMS_cs
+{
+    public partial class LoginForm : Form
+    {
+        private SqlConnection conn;
+        private string connectionString = "Server=localhost;Database=LibraryDB;Trusted_Connection=True;";
+
+        public LoginForm()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            // You can keep this empty or add initialization logic if needed
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                string query = "SELECT * FROM Users WHERE Username = @Username AND Password = @Password";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                    cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string role = reader["Role"].ToString();
+                            int userId = Convert.ToInt32(reader["Id"]);
+
+                            if (role.Equals("Admin", StringComparison.OrdinalIgnoreCase))
+                            {
+                                new AdminDashboard(userId).Show();
+                            }
+                            else
+                            {
+                                new MemberDashboard(userId).Show();
+                            }
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid credentials!");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
+
